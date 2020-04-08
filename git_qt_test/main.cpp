381496903011146b185bd6c9e7bf60c33fb30e1c
ilya-uzun <ilya.uzun@bk.ru>
2020-04-08 15:13:44 +0500

    Update Lab5

--- git_qt_test/main.cpp
+++ git_qt_test/main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
+// Lab3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
 
+#include <iostream>
+#include "Time.h"
+#include <windows.h>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+
+int main(){
+    SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
+    SetConsoleOutputCP(1251);
+    Time a;//конструктор без параметров
+    Time b;//конструктор без параметров
+    Time c;//конструктор без параметров
+    cin >> a;
+    cin >> b;
+    ++a;// префикная операция инкремент
+    cout << a << endl;
+    c = (a++) + b;// сложение и постфиксная операция инкремент
+    cout << "a= " << a << endl;
+    cout << "b= " << b << endl;
+    cout << "c= " << c << endl;
+
 }
--- git_qt_test/time.cpp
+++ git_qt_test/time.cpp
@@ -1,6 +1,57 @@
-#include "time.h"
+#include <iostream>
+#include "Time.h"
+using namespace std;
 
-Time::Time()
-{
+// перегрузка операции присваивания
+Time &Time::operator=(const Time &t) {
+    //перегрузка на само присваивание
+    if (&t == this) return *this;
+    min = t.min;
+    sec = t.sec;
+    return *this;
+}
 
+// перегрузка префиской операции инкремент
+Time &Time::operator++() {
+
+    int temp = min * 60 + sec;
+    temp++;
+    min = temp / 60;
+    sec = temp % 60;
+    return *this;
+}
+
+// перегрузка постфиксная операции инкремент
+Time Time::operator ++ (int) {
+
+    int temp = min * 60 + sec;
+    temp++;
+    Time t(min, sec);
+    min = temp / 60;
+    sec = temp % 60;
+    return t;
+}
+
+//Перегрузка бинарной операции сложения
+Time Time::operator+(const Time& t) {
+    int temp1 = min * 60 + sec;
+    int temp2 = t.min * 60 + t.sec;
+    Time p;
+    p.min = (temp1 + temp2) / 60;
+    p.sec = (temp1 + temp2) % 60;
+    return p;
+}
+
+//Перегрузка глобальной функции-оператора ввода
+istream& operator >> (istream& in, Time& t) {
+    cout << "min ? ";
+    in >> t.min;
+    cout << "sec ? ";
+    in >> t.sec;
+    return in;
+}
+
+//перегрузка глобальной функции-оператора вывода
+ostream& operator << (ostream& out, const Time& t) {
+    return (out << t.min << " : " << t.sec );
 }
