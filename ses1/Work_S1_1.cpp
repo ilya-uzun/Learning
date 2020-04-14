02d6ccc7f6af272632dbadc1d7cbb484046d14ae
ilya-uzun <https://github.com/ilya-uzun>
2020-04-14 16:27:20 +0500

    Update 14 04 2020

--- ses1/Work_S1_1.cpp
+++ ses1/Work_S1_1.cpp
@@ -11,7 +11,7 @@ float d = pow(a, 3) + 3*a*a*b;
 float e = 3*a*b*b + pow(b, 3);
 float f = (c - d) / e;
 
-cout <<"type float: "<< f <<endl;
+cout <<"type float: "<< f << endl;
 
 double r = 1000, g = 0.0001;
 double h = pow((r + g), 3);
@@ -19,7 +19,7 @@ double j = pow(r, 3) + 3*r*r*g;
 double k = 3*r*g*g + pow(g, 3);
 double dr = (h - j) / k;
 
-cout << "type double:" << dr <<endl;
+cout << "type double:" << dr << endl;
 
 return 0;
 }
