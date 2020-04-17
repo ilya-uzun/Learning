946a262b80c0a553559e00378678979d6ddde3fa
ilya-uzun <ilya.uzun@bk.ru>
2020-04-17 06:00:44 +0500

    Update Makefile

--- ses2/job3/Time.h
+++ ses2/job3/Time.h
@@ -5,6 +5,8 @@ using namespace std;
 
 class Time{
 	int min, sec;
+
+public:
 	Time()
 	{
 		min = 0; sec = 0;
@@ -18,18 +20,18 @@ class Time{
 		min = t.min; sec = t.sec;
 	}
 	~Time(){}
-public:
-	//int get_min() { return min; }
-	//int get_sec() { return sec; }
-	//void set_min(int m) { min = m; }
-	//void set_sec(int s) { sec = s; }
+
+	int get_min() { return min; }
+	int get_sec() { return sec; }
+	void set_min(int m) { min = m; }
+	void set_sec(int s) { sec = s; }
 
 	//перегруженные операторы
-	Time& operator = (const Time&); // опирация присваивания
-	Time& operator ++ (); //
+	Time &operator = (const Time&); // опирация присваивания
+	Time &operator ++ (); //
 	Time operator ++ (int);// постфиксная операция
 	Time operator + (const Time&);
 	//Перегрузить операции ввода и вывода объектов с помощью потоков.
-	 istream &operator >> (istream&in, Time &t);
-	 ostream &operator << (ostream&out, const Time& t);
+	friend istream &operator >> (istream&in, Time &t);
+	friend ostream &operator << (ostream&out, const Time& t);
 };
--- ses2/job3/main.cpp
+++ ses2/job3/main.cpp
@@ -4,27 +4,36 @@
 using namespace std;
 
 
-int main(){
-    SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
-    SetConsoleOutputCP(1251);
-    Time a;//конструктор без параметров
-    Time b;//конструктор без параметров
-    Time c;//конструктор без параметров
-    Time d;//конструктор без параметров
-    // cout << "Класс a - " << endl;
-    // cout << "Введите минуты - "<< endl;
-    // a.get_min();
-    // cout << "Введите секунды - " << endl;
-    // a.get_sec();
-    //cin >> a;
-    cout << "Класс b - " << endl;
-    cin >> b;
+int main()
+{
+  SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
+  SetConsoleOutputCP(1251);
+  Time a;//конструктор без параметров
+  Time b;//конструктор без параметров
+  Time c;//конструктор без параметров
 
-    ++a;// префикная операция инкремент
-    cout << a << endl;
-    c = (a++) + b;// сложение и постфиксная операция инкремент
-    cout << "a= " << a << endl;
-    cout << "b= " << b << endl;
-    cout << "c= " << c << endl;
-    cout << "d= " << d << endl;
+  cout << "Класс a - " << endl;
+  cout << "Введите минуты - "<< endl;
+  a.get_min();
+  cout << "Введите секунды - " << endl;
+  a.get_sec();
+  cin >> a;
+  cout << "Класс b - " << endl;
+  cin >> b;
+
+  ++a;// префикная операция инкремент
+  cout << a << endl;
+  c = (a++) + b;// сложение и постфиксная операция инкремент
+  cout << "a= " << a << endl;
+  cout << "b= " << b << endl;
+  cout << "c= " << c << endl;
+  //cout << "d= " << d << endl;
+
+/*----------   Компоненты set и get      ---------*/
+  cout << " " << min_ <<'\n';
+  Time d {10, 0};//конструктор без параметров
+  d.set_min(32);// задание через модификаторы
+  int min_ = d.get_min();
+  cout << "вывод get_min " << min_ <<'\n';
+
 }
