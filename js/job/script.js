bfd8e2b789d40a0d13bdc6c15d83074039c06c40
ilya-uzun <ilya.uzun@bk.ru>
2020-04-22 04:38:22 +0500

    Update 22 04 2020 9:38

--- js/job/script.js
+++ js/job/script.js
@@ -1,8 +1,52 @@
 Vue.component('taks'{
   props:['data'],
-  data(){
-    return {
-
+  methods: {
+    task_done(){
+      this.$emit('task_done');
     }
-  }
+ },
+  template: `
+  <div class="task">
+    <div>
+      <h3 class="task__title">{{data.title}}</h3>
+      <p class="task__desc">{{data.desc}}</p>
+    </div>
+    <button @click="task_done()" class="task__done">#</button>
+  </div>
+  `
+})
+
+var vue = new Vue({
+  el: '#app',
+  data:{
+    new_task:{
+      title: '',
+      desc: ''
+    },
+    tasks: [
+      {
+        title: 'Изучить Vue.js',
+        desc: 'Прочитать книгу "Величие Vue.js 2"'
+      },
+      {
+        title: 'Записать видио',
+        desc: 'Расказать  про vuex и vue-router'
+      },
+    ]
+  },
+  methods: {
+    delete_task(id){
+      this.tasks.splite(id, 1);
+    }
+    add_task(){
+        if(this.new_task.title!=''){
+          this.tasks.push({
+            title: this.new_task.title,
+            desc: this.new_task.desc
+          });
+          this.new_task.title='';
+          this.new_task.desc='';
+        }
+    }
+ }
 })
