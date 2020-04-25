552a1661f3352dd6994de9883935a08330622771
ilya-uzun <ilya.uzun@bk.ru>
2020-04-25 21:09:20 +0500

    Job2 пока самый нормальный вариант

--- js/job2/script.js
+++ js/job2/script.js
@@ -1,51 +1,54 @@
-var list = document.querySelector('ul');
+
 //Добавление локального хранилища
 var saveLine;
 function toLocal() {
   saveLine = list.innerHTML;
   localStorage.setItem('saveLine', saveLine);
 }
-
+var list = document.querySelector('ul');
 list.addEventListener('click', function (ev){
-  if(ev.target.tagName === "LI"){
-      ev.target.classList.toggle('checked');
-;
+  if(ev.target.tagName === "LI"){// если кликнули по элементу списка
+      ev.target.classList.toggle('checked');//Устаноавливаем класс 'checked'элементу списка 
       toLocal();
-  } else if(ev.target.tagName === "SPAN"){
+}else if(ev.target.tagName === "SPAN"){//
       var div = ev.target.parentNode;
       div.remove();
       toLocal();
   }
 }, false);
 
-// Добавление в список дел
+// ввод по enter
 function checkKey(event){
   if(event.keyCode == "13"){
     newElement();
   }
 }
+// Добавление в список дел
 function newElement() {
   var li = document.createElement('li');
-  var inputValue = document.getElementById('newCase').value;
+  var inputValue = document.getElementById('newCase').value;// получаем значения поля ввода
   var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+  li.appendChild(t);// сформирован элемент списка
+
   if(inputValue == ""){
     alert("Введите ваше дело!")
   }else{
-    document.getElementById('list').appendChild(li);
+    document.getElementById('list').appendChild(li);// добавляем в список дел
   }
+  // очистка значения списка
   document.getElementById('newCase').value = "";
-  var done = document.createElement('DONE');//создать новый элемент
-  var doneText = document.createTextNode("Выполнено");
-  done.className = "done";
-  done.insertBefore(doneText, );//
-  li.insertBefore(done); 
-
   var span = document.createElement('SPAN');//создать новый элемент
   var txt = document.createTextNode("Удалить");
   span.className = "close";
   span.appendChild(txt);//
   li.appendChild(span);
+
+  var done = document.createElement('span');//создать новый элемент
+  var txtDone = document.createTextNode("Не выполнено");
+  done.className = "done";
+  done.appendChild(txtDone);//
+  li.appendChild(done);
+
   toLocal();
 }
 
