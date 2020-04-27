e73b380d2c900fa437e26eeef6f8f905076bf4ce
ilya-uzun <ilya.uzun@bk.ru>
2020-04-27 21:37:18 +0500

    Update рабочий метод сортировки прузьков

--- C++/Bubble_method/main.cpp
+++ C++/Bubble_method/main.cpp
@@ -6,33 +6,37 @@ int main()
     int *arr;
     int size;
 
-    cont<<"n = ";
+    cout<<"n = ";
     cin >> size;
 
     if (size <= 0)
-    {
+    {//проверка, чтобы массив небыл отрицателным
         cerr<<"Invalid size"<< endl;
         return 1; //Завершить програссу с ошибкой
     }
     arr = new int[size];
 
-    for(int i=0; i< size; i++)
-    {
+    for(int i=0; i < size; i++)
+    {// вводим массив
         cout << "arr[" << i << "] = ";
         cin >> arr[i];
     }
-    int temp;
+    
     for(int i = 0; i < size-1; i++)
     {
-        for(int j=0; j<size - i - 1; j++)
+        for(int j=0; j < size-i-1; j++)
         {
-            temp = arr[j];
+            if(arr[j]>arr[j+1])
+            {
+            int temp = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = temp;
+            }
+
         }
     }
-    for(int i=0; i<size; i++)
-    {
+    for(int i=0; i < size; i++)
+    {// выводим массив
         cout << arr[i] << " ";
     }
     cout << endl;
