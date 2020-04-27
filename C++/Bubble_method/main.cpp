2ea5bf1ea81b8269ec341b28660dc6bc92a918a6
ilya-uzun <ilya.uzun@bk.ru>
2020-04-27 21:40:14 +0500

    Updata сортировка методам пузырьков внёс комментарии

--- C++/Bubble_method/main.cpp
+++ C++/Bubble_method/main.cpp
@@ -21,13 +21,13 @@ int main()
         cout << "arr[" << i << "] = ";
         cin >> arr[i];
     }
-    
+    // cортировка массива пузырьков
     for(int i = 0; i < size-1; i++)
     {
         for(int j=0; j < size-i-1; j++)
         {
             if(arr[j]>arr[j+1])
-            {
+            {//меняем элеенты местами
             int temp = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = temp;
@@ -36,7 +36,7 @@ int main()
         }
     }
     for(int i=0; i < size; i++)
-    {// выводим массив
+    {// выводим массива
         cout << arr[i] << " ";
     }
     cout << endl;
