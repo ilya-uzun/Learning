464985f4b1e912d880ac0897d1f6a5a648b31c0c
ilya-uzun <ilya.uzun@bk.ru>
2020-05-05 23:07:58 +0500

    job7 Рабочая перенёс реализацию в main

--- ses2/job7/Vector.h
+++ ses2/job7/Vector.h
@@ -26,22 +26,72 @@ class Vector
 {
 public:
 	//конструктор с параметрами: выделяет паметь под s элемент и заполняет ихзначение к
-	 Vector(int s,T k);
+	 Vector(int s,T k)
+	 {
+		 size=s;
+data=new T[size];
+for(int i=0;i<size;i++)
+data[i]=k;
+	 }
 	//конструкор копирования 
-	 Vector(const Vector<T> &a);
+	 Vector(const Vector<T> &a)
+	 {
+		 	size = a.size;
+	data = new T[size];
+	for (int i = 0; i < size; i++) 
+		data[i] = a.data[i];
+	 }
 	//конструктор с параметрами
-	~Vector();
+	~Vector()
+	{
+	delete[]data;
+	data = 0;
+	}
 	//деструктор
-	Vector& operator=(const Vector<T> &a);
+	Vector& operator=(const Vector<T> &a)
+	{
+		if (this == &a) return (T)(*this);
+	size = a.size;
+	if (data != 0) delete[]data;
+	data = new T[size];
+	for (int i = 0; i < size; i++)
+		data[i] = a.data[i];
+	// beg = a.beg;
+	// end = a.end;
+	return (T)(*this);	
+	}
 	//операция присваивания
-	T& operator[](int index);
+	T& operator[](int index)
+	{
+		if (index<size) return (T) (data[index]);
+		else std::cout<<"\nError! Index>size";
+	}
 	//операция доступа по индексу
-	Vector operator+(const T k);
+	Vector operator+(const T k)
+	{
+	Vector<T> temp(size, k);
+	for (int i = 0; i < size; i++)
+		temp.data[i] += data[i]+k;
+	return (T)temp;
+	}
 	//операция для добавления константы
-	int operator()();
+	int operator()()
+	{
+		return size;
+	}
 	// перегруженные операции ввода-вывода
-	template <typename T1> friend std::ostream& operator << (std::ostream& out, const Vector<T1> &a);
-	template <typename T1> friend std::istream& operator >> (std::istream& in, Vector<T1> &a);
+	template <typename T1> friend std::ostream& operator << (std::ostream& out, const Vector<T1> &a)
+	{
+			for (int i = 0; i < a.size; ++i)
+		out << a.data[i]<< " ";
+	return (T)out;
+	}
+	template <typename T1> friend std::istream& operator >> (std::istream& in, Vector<T1> &a)
+	{
+		for (int i = 0; i < a.size; ++i)
+		in >> a.data[i];
+	return (T)in;
+	}
 
 	//Iterator first() { return beg; } //возвращает указатель на первый элемент
 	//Iterator last() { return end; } // возвращает ук. следующий за последним
--- ses2/job7/main.cpp
+++ ses2/job7/main.cpp
@@ -1,8 +1,91 @@
 ﻿#pragma once
 #include "Time.h"
-#include "Vector.h"
+//#include "Vector.h"
 #include <iostream>
 using namespace std;
+
+template <class T>
+class Vector
+{
+public:
+	//конструктор с параметрами: выделяет паметь под s элемент и заполняет ихзначение к
+	 Vector(int s,T k)
+	 {
+		 size=s;
+data=new T[size];
+for(int i=0;i<size;i++)
+data[i]=k;
+	 }
+	//конструкор копирования 
+	 Vector(const Vector<T> &a)
+	 {
+		 	size = a.size;
+	data = new T[size];
+	for (int i = 0; i < size; i++) 
+		data[i] = a.data[i];
+	 }
+	//конструктор с параметрами
+	~Vector()
+	{
+	delete[]data;
+	data = 0;
+	}
+	//деструктор
+	Vector& operator=(const Vector<T> &a)
+	{
+		if (this == &a) return (T)(*this);
+	size = a.size;
+	if (data != 0) delete[]data;
+	data = new T[size];
+	for (int i = 0; i < size; i++)
+		data[i] = a.data[i];
+	// beg = a.beg;
+	// end = a.end;
+	return (T)(*this);	
+	}
+	//операция присваивания
+	T& operator[](int index)
+	{
+		if (index<size) return (T) (data[index]);
+		else std::cout<<"\nError! Index>size";
+	}
+	//операция доступа по индексу
+	Vector operator+(const T k)
+	{
+	Vector<T> temp(size, k);
+	for (int i = 0; i < size; i++)
+		temp.data[i] += data[i]+k;
+	return (T)temp;
+	}
+	//операция для добавления константы
+	int operator()()
+	{
+		return size;
+	}
+	// перегруженные операции ввода-вывода
+	template <typename T1> friend std::ostream& operator << (std::ostream& out, const Vector<T1> &a)
+	{
+			for (int i = 0; i < a.size; ++i)
+		out << a.data[i]<< " ";
+	return out;
+	}
+	template <typename T1> friend std::istream& operator >> (std::istream& in, Vector<T1> &a)
+	{
+		for (int i = 0; i < a.size; ++i)
+		in >> a.data[i];
+	return in;
+	}
+
+	//Iterator first() { return beg; } //возвращает указатель на первый элемент
+	//Iterator last() { return end; } // возвращает ук. следующий за последним
+
+private:
+	int size;//размер вектора
+	T* data;// указатель на денамический массив значеий вектора
+	//Iterator beg;//указатель на первый элемент
+	//Iterator end;// уазатель на элемент следущий за последним
+
+};
 int main()
 {
    //инициализация, ввод и вывод значений вектора
@@ -10,8 +93,8 @@ int main()
    cin>>t;
    cout<<t;
    Vector <int>  A(5,0);//создали вектор из 5 элементов, заполненые нулями 
-   //cout << "\n Input Vector A = " << A << endl;
-   //cin >> a;//ввели с клавиатуры значения элементов вектора(ошибка компиляции считает что cin вводит char)
+   cout << "\n Input Vector A = " << A << endl;
+   cin >> A;//ввели с клавиатуры значения элементов вектора(ошибка компиляции считает что cin вводит char)
    //cout << A << "\n";//вывели значения элементов вектора
    //  Vector <int> b(3);//создали вектор из 5 элементов, заполненые нулями 
    //  cout << "Input Vector B " << endl;
