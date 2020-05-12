bb96e9cc0d23bfda856f96f7518fc0d5de412061
ilya-uzun <ilya.uzun@bk.ru>
2020-05-12 22:53:06 +0500

    Тестирут класс без констркторов

---
+++ C++/Class_by_efault/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Student
+{
+    string name;
+    int age;
+public:
+    string set_name(string s)
+    {
+        name = s;
+        return name;
+    };
+    int set_age(int)
+    {
+
+    };
+
+}
+int main()
+{
+
+
+	return 0;
+}
