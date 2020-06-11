7e884a0f89dfc48612aed5e87927e16e7c1c90c7
ilya-uzun <ilya.uzun@bk.ru>
2020-06-11 22:12:17 +0500

    Updata Palindrome.js

--- js/test/Palindrome.js
+++ js/test/Palindrome.js
@@ -1,8 +1,17 @@
-function polindrom (str){
+// стрелочная функция
+const polindrom =str =>{ 
     //перевод строки в нижний регистр
     str = str.toLowerCase();
-    // перевод строки в массив
-    let str2 = str.split('');
-    return str2;
+    return str == str.split('').reverse().join('');// сранение
+
+    // неудачный способ
+    // // перевод строки в массив
+    // let str2 = str.split('');
+    // // развернуть строку
+    // str2 = str2.reverse(); // разварачивает массив
+    // str2 = str2.join(''); // обратная опираия  split
+    // if (str == str2) return true;
+    // else return false;
+    // return str2;
 }
-console.log(polindrom('Hello'));
+console.log(polindrom('AbbeA'));
