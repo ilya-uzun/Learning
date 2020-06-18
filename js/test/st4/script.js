78203a876f7ef48814fedd71a4cfd56cb62d48be
ilya-uzun <ilya.uzun@bk.ru>
2020-06-18 21:29:50 +0500

    Update 18.60.2020 21:39

--- js/test/st4/script.js
+++ js/test/st4/script.js
@@ -1,3 +1,20 @@
-function Hello(){
-  alert('вы нажали')
+const inHours = 20;
+
+if (inHours < 12) 
+{
+  console.log('Доброе утро! if')
+} else if (inHours >= 12 && inHours < 18)
+{
+  console.log('Доброе утро! else')
+} else if (inHours <= 18 && inHours < 22)
+{
+  console.log('Доброе утро! else2')
+} else 
+{
+  console.log('Доброй ночи')
 }
+
+
+// function Hello(){
+//   alert('вы нажали')
+// }
