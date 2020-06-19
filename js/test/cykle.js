b885cb5af85b326189cbc73099fd7faa3411fc12
ilya-uzun <ilya.uzun@bk.ru>
2020-06-19 17:55:28 +0500

    new cykle.js

---
+++ js/test/cykle.js
@@ -0,0 +1,13 @@
+var i;
+for(i = 0; i < 10; i++)
+{
+    i++;
+    console.log(i);
+}
+
+// var a = false;
+// var b = false;
+
+// if (a || b) {
+//     console.log('основное условие');
+// } else console.log(' else');
