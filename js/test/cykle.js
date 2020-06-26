fd9ef6d13bf9a54f3eb9dbe59f0f09771f5664a6
ilya-uzun <ilya.uzun@bk.ru>
2020-06-26 22:29:32 +0500

    тесты над циклами

--- js/test/cykle.js
+++ js/test/cykle.js
@@ -1,9 +1,9 @@
-var i;
-for(i = 0; i < 10; i++)
-{
-    i++;
-    console.log(i);
-}
+// var i;
+// for(i = 0; i < 10; i++)
+// {
+//     i++;
+//     console.log(i);
+// }
 
 // var a = false;
 // var b = false;
@@ -11,3 +11,16 @@ for(i = 0; i < 10; i++)
 // if (a || b) {
 //     console.log('основное условие');
 // } else console.log(' else');
+
+// выход изцикла имея только дме отмеченные строки
+
+for (let i = 0; i < 10; i++){
+    i++;
+    document.getElementById("i").innerHTML = i;
+    for (let j = 0; j < 11; j++){
+        j++; 
+        document.getElementById("j").innerHTML = j;
+     }     // 
+
+}
+
