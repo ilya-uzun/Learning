1fe041f09f8fa49e6725d3afa3a1be11e770c211
ilya-uzun <ilya.uzun@bk.ru>
2020-07-09 20:30:54 +0500

    добавил address.cpp

---
+++ C++/test/address.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void f (int i, int* j, int& k)
+{
+  i++;
+  (*j)++;
+  k++;
+};
+
+int main ()
+{
+int i = 1, j = 2, k = 3;
+cout <<" i j k\n";
+cout << i << j << k << endl;
+f (i, &j, k);
+cout << i << j << k << endl;
+return 0;
+}
