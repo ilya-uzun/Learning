b00caa31b90c997e03dfc153682480c9ce53e22c
ilya-uzun <ilya.uzun@bk.ru>
2020-07-15 21:31:25 +0500

    Update 21 31

--- C++/qt/TestFormQT/main.cpp
+++ C++/qt/TestFormQT/main.cpp
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    // test metod summe_nuber
+// test metod summe_nuber
 //    int b = 3;
 //    int c = 5;
 //    ClassMath test;
 //    //выводим в leber результат метода summe_number
 //    w.setLabelText(test.summe_number(b,c));
 
-    // тест Edit
+// тест Edit
 //    int n1;
 //    QString n1 = w.get_TextEdit1();
 //    w.setLabelText("ww");
