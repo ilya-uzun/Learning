ee210e8b058b2bef1b5a0eb522186c045f4b68ba
ilya-uzun <ilya.uzun@bk.ru>
2020-07-25 21:22:54 +0500

    shake update

--- C++/qt/Snake/snake/snake.cpp
+++ C++/qt/Snake/snake/snake.cpp
@@ -1,6 +1,77 @@
 #include "snake.h"
+#include <QPainter>
+#include <QTime>
 
 snake::snake(QWidget *parent) : QWidget(parent)
 {
+    setStyleSheet("background-color:black;");
+    leftDirection = false;
+    rightDirection = true;
+    upDirection = false;
+    downDirection = false;
+    inGame = true;
 
+    resize(B_WIDTH, B_HEIGHT);
+    loadImages();
+    initGame();
+}
+
+void snake::loadImages()
+{
+    dot.load("dot.png");
+    head.load("head.png");
+    apple.load("apple.png");
+}
+
+void snake::initGame()
+{
+    dots = 3;
+
+    for (int z = 0; z < dots; z++)
+    {
+        x[z] = 50 - z * 10;
+        y[z] = 50;
+    }
+
+    locateApple();
+    timeID = startTimer(DELAY);
+
+}
+
+void snake::doDrawing()
+{
+    QPainter qp(this);
+
+    if(inGame)
+    {
+        qp.drawImage(apple_x, apple_y, apple);
+
+        for (int z = 0; z < dots; z++)
+        {
+            if (z == 0){
+                qp.drawImage(x[z], y[z], head);
+            } else{
+                qp.drawImage(x[z], y[z], dot);
+            }
+        }
+
+    }else {//if(inGame)
+        gameOver(qp);
+    }
+}
+
+void snake::checkCollision()
+{
+    for (int z = dots; z > 0; z--) {
+        x[z]= x[(z-1)];
+        y[z]= y[(z-1)];
+    }
+
+    if(leftDirection) x[0] -= DOT_SIZE;
+
+    if(rightDirection) x[0] += DOT_SIZE;
+
+    if(upDirection) y[0] -= DOT_SIZE;
+
+    if(downDirection) y[0] += DOT_SIZE;
 }
--- C++/qt/Snake/snake/snake.h
+++ C++/qt/Snake/snake/snake.h
@@ -2,13 +2,51 @@
 #define SNAKE_H
 
 #include <QWidget>
+#include <QKeyEvent>
 
 class snake : public QWidget
 {
 public:
     explicit snake(QWidget *parent = nullptr);
 
-signals:
+protected: // производные классы получают свободный доступ
+    void paintEvent(QPaintEvent *);
+    void timerEvent(QTimerEvent *);
+    void keyPressEvent(QKeyEvent *);
+private:
+    QImage dot;
+    QImage head;
+    QImage apple;
+
+    static const int B_WIDTH = 300;
+    static const int B_HEIGHT = 300;
+    static const int DOT_SIZE = 10;
+    static const int ALL_DOTS = 900; // размеры яблока
+    static const int RAND_POS = 29; // случайная позиция яблока
+    static const int DELAY = 140; // скорость игры
+
+    int timeID;
+    int dots;
+    int apple_x;
+    int apple_y;
+    // содержат координаты (x;y) всех «частей» тела змеи
+    int x[ALL_DOTS];
+    int y[ALL_DOTS];
+
+    bool leftDirection;
+    bool rightDirection;
+    bool upDirection;
+    bool downDirection;
+    bool inGame;
+
+    void loadImages();
+    void initGame();
+    void locateApple();
+    void checkApple();
+    void checkCollision();
+    void move();
+    void doDrawing();
+    void gameOver(QPainter &);
 
 };
 
