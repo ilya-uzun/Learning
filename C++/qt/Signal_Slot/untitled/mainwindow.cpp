10cb2f418ddb9a9fb5c105530aba5cda8d11e41e
Илья <63155683+ilya-uzun@users.noreply.github.com>
2020-09-01 11:47:47 +0500

    Update mainwindow.cpp

--- C++/qt/Signal_Slot/untitled/mainwindow.cpp
+++ C++/qt/Signal_Slot/untitled/mainwindow.cpp
@@ -6,10 +6,51 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+        
+    // инициализация кнопок
+    QPushButton *but_1 = new QPushButton(this);
+    QPushButton *but_2 = new QPushButton(this);
+    QPushButton *but_3 = new QPushButton(this);
+    
+    // Устанавливаем номера кнопок
+    but_1->setText("1");
+    but_1->setText("2");
+    but_1->setText("3");
+    
+    // Добовляем кнопки на слои с вертикальной ориентацией
+    ui->verticalLayout->addWidget(but_1);
+    ui->verticalLayout->addWidget(but_2);
+    ui->verticalLayout->addWidget(but_3);
+    
+    //Подключаем к напкам индивидуальные слоты
+    connect(but_1, SIGNAL(clicked()), this, SLOT(slotButton1()));
+    connect(but_2, SIGNAL(clicked()), this, SLOT(slotButton2()));
+    connect(but_3, SIGNAL(clicked()), this, SLOT(slotButton3()));
+    
+    // Подключаем сигнал с передачей номера кнопки к слоту вывода сообщения
+    connect(this, &MainWindow::signalFromButton, this, &MainWindow::slotMessage);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+//Слоты для обработки нажатия кнопок
+
+void MainWindow::slotButton1()
+{
+    emit signalFromButton(1);
+}    
+
+void MainWindow::slotButton2()
+{
+    emit signalFromButton(2);
+} 
+
+void MainWindow::slotButton3()
+{
+    emit signalFromButton(3);
+} 
+
 
