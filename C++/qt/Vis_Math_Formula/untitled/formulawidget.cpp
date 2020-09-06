34a6d81257537beb51d0161597c71532e95d3f60
ilya-uzun <ilya.uzun@bk.ru>
2020-09-06 13:03:03 +0500

    набросал код

--- C++/qt/Vis_Math_Formula/untitled/formulawidget.cpp
+++ C++/qt/Vis_Math_Formula/untitled/formulawidget.cpp
@@ -1,10 +1,73 @@
 #include "formulawidget.h"
 
-FormulaWidget::FormulaWidget()
-{
 
+const QString FormulaItem::RUGULAR_EXPRESSION = "sqrt\\((?<value>))";
+/*
+Статическая постоянная переменная REGULAR_EXPRESSION является строкой,
+которая содержит регулярное выражение для поиска вхождения формулы
+*/
+QPoint FormulaItem::draw(const QPoint& pos, QPainter& p) const
+{
+  int valueWidht = p.fontMetrics().width(m_value);
+  int valueHeight = p.fontMetrics().height();
+
+  p.drawLine(pos.x(), 4 + valueHeight / 2, pos.x() + 5, 4 + valueHeight);
+  p.drawLine(pos.x() + 5, 4 + valueHeight, pos.x() + 10, + pos.y() + 1);
+  p.drawLine(pos.x() + 10, pos.y() + 1, pos.x() + 14 + valueWidht, pos.y() + 1);
+
+  p.drawText(QRect(pos.x() + 12, pos.y() + 4, pos.x() + 12 + valueWidht, pos.y() + 4 + valueHeight),m_value);
+  return QPoint(pos.x() + valueWidht + 20, pos.y());
 }
 
-const QString FormulaItem::RUGULAR_EXPRESSION = "sqrt\\((?<value>))"
 
-QPoint FormulaItem::draw(const QPoint& pos)
+FormulaWidget::FormulaWidget(QWidget* parent) :
+    BaseClass(parent)
+{
+    //Установим цвет фона виджета, по умолчанию он такой же, как в системном оформлении ОС
+    QPalette pal = palette();
+    pal.setColor(QPalette::Background, Qt::white);
+    setAutoFillBackground(true);
+    setPalette(pal);
+}
+
+void FormulaWidget::setFormula(const QString& formula)
+{
+    //// Очищаем все формулы
+    m_items.clear();
+
+    // Создаём объект регулярного выражения для поиска формулы
+    QRegularExpression sqrt_value(FormulaItem::RUGULAR_EXPRESSION);
+    // Ищем все вхождения формулы
+    QRegularExpressionMatchIrerator i = sqrt_value.globalMatch(formula);
+
+    // создаём все объекты формул
+    while (i.hasNext())
+    {
+        QRegularExpressionMatch match = i.next();
+        if (match.hasMatch())
+        {
+            m_items.append(FormulItem(match.captured("value")));
+        }
+    }
+
+    // Запускаем перерисовку
+    update();
+}
+
+void FormulaWidget::paintEvent(QPaintEvent *event)
+{
+    // Для перерисовки используется объект QPainter,
+    // который обязательно должен получить объект за отрисовку которого он отвечает
+    QPainter p(this);
+    p.setRenderHint(QPainter::Antialiasing);
+    p.setPen(Qt::black);
+
+    QPoint formulaPos(2, 2);
+
+    // Производим отрисовку всех формул, которые удалось найти
+    for (const FormulaItem& item : m_items)
+    {
+        formulaPos = item.draw(formulaPos, p);
+    }
+
+}
--- C++/qt/Vis_Math_Formula/untitled/formulawidget.h
+++ C++/qt/Vis_Math_Formula/untitled/formulawidget.h
@@ -1,9 +1,15 @@
 #ifndef FORMULAWIDGET_H
 #define FORMULAWIDGET_H
 
+#include <QString>
+#include <QPoint>
+#include <QPainter>
+#include <QMainWindow>
+
 // Класс визуализации формулы
 class FormulaItem
 {
+
  public:
   explicit FormulaItem(QString value): m_value(value){}
 
@@ -11,9 +17,28 @@ class FormulaItem
   //Метод отрисовки
   QPoint draw(const QPoint& pos, QPainter& p) const;
 
- privte:
+ private:
   QString m_value;// Значение форулы
-}
+};
+
+// класс для отрисовки всех формул
+class FormulaWidget : public QMainWindow
+{
+    Q_OBJECT
+    using BaseClass = QWidget;
+public:
+    explicit FormulaWidget(QWidget* parent = nullptr);
+
+public slots:
+    //Слот для установки формулы
+    void setFormula(const QString& formula);
+
+protected:
+    virtual void paintEvent (QPaintEvent* event) override;
+
+private:
+    QList<FormulaItem> m_items;
+};
 
 
 #endif // FORMULAWIDGET_H
