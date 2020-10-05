f1715d603640b174eab5757bdee97f5c5f22bb21
ilya-uzun <ilya.uzun@bk.ru>
2020-10-05 17:57:54 +0500

    Исправляю ошибки основного класса

--- C++/qt/Lab16_V2/lab16_v2/mainwindow.cpp
+++ C++/qt/Lab16_V2/lab16_v2/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    tree = BinaryTree(ui->treeView);
+    tree.initializeWithStartValue();
+   // ui->msgBox->setText("");
+    setFixedSize(width(), 725);
 }
 
 MainWindow::~MainWindow()
@@ -16,18 +21,68 @@ MainWindow::~MainWindow()
 // Поиск элемента
 void MainWindow::on_searchBt_clicked()
 {
-
+   // try {
+       // double value = InputDialog::getDouble(this, "Введите значение, которое нужно найти!");
+        try {
+            Node *node = tree.search(value);
+            tree.highlightNode(node);
+            showMessage(QString("Элемент %1 найден!").arg(node->data));
+            return;
+        } catch (exception &e) {
+            showMessage(e.what());
+        }
+   // } catch (exception &e) {
+    //    showMessage(e.what());
+    }
+    tree.clearHighlight();
 }
-
 
 //Добавить элемент
 void MainWindow::on_addBt_clicked()
 {
-
+    double value;
+    try {
+        value = InputDialog::getDouble(this, "Введите значение, которое нужно добавить!");
+        try {
+            tree.insert(value);
+            showMessage(QString("Элемент %1 был добавлен!").arg(value));
+        } catch (exception &e) {
+            showMessage(e.what());
+        }
+    } catch (exception &e) {
+        shoowMessage(e.what());
+    }
 }
 
 //Удалить элемент
 void MainWindow::on_deleteBt_clicked()
 {
-
+    double value;
+    try {
+        value = value = InputDialog::getDouble(this, "Введите значение, которое нужно удалить!");
+        try {
+            tree.remove(value);
+            showMessage(QString("Элемент %1 был удален!").arg(value));
+        } catch (exception &e) {
+            showMessage(e.what());
+        }
+    } catch (exception &e) {
+        showMessage(e.what());
+    }
 }
+
+/**
+ * Метод для вывода сообщения в данном виджете.
+ * Выводит переданный текст на специальной панели сообщений.
+ */
+void MainWindow::showMessage(QString msg) {
+    ui->msgBox->setText(msg);
+}
+
+/**
+ * Метод для очищения панели сообщений от текста.
+ */
+void MainWindow::clearMessages() {
+    ui->msgBox->clear();
+}
+?
--- C++/qt/Lab16_V2/lab16_v2/mainwindow.h
+++ C++/qt/Lab16_V2/lab16_v2/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QInputDialog>
+#include <iostream>
+#include "binarytree.h"
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -10,20 +14,22 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
+    Ui::MainWindow *ui; //Указатель на интерфейс
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private:
+    BinaryTree tree;//Поле с бинарным деревом, над которым проводятся все операции ползователя
+    void showMessage(QString msg);//Метод для вывода сообщения в данном виджете
+    void clearMessages();//Метод для очищения панели сообщений от текста
 
 private slots:
     void on_searchBt_clicked(); // Поиск элемента
-
     void on_addBt_clicked(); //Добавить элемент
-
     void on_deleteBt_clicked(); //Удалить элемент
 
-private:
-    Ui::MainWindow *ui;
+protected:
+    void mousePressEvent(QMouseEvent *event) override;  //Метод-обработчик события нажатия на кнопку мыши
 };
 #endif // MAINWINDOW_H
