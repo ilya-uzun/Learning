05c49818c2ddc2cdfbfb091400828e7b9e8db10a
Илья <63155683+ilya-uzun@users.noreply.github.com>
2020-10-13 19:37:08 +0500

    Update Man.cpp
    Делал на работе

--- C++/Task1_1/Man.cpp
+++ C++/Task1_1/Man.cpp
@@ -19,17 +19,21 @@ Man::SetName(const char* fromBuf){
     pName[l_name] = 0;
 }
 
-Man::Man(int lName){
-    cout << "Constructor is working" << endl;
-    pName = new char[lName + 1 ];
+Man::SetBirthYear(const char* fromBuf){
+    birth_year = atoi(fromBuf + l_name); // Узнать про atoi
 }
 
-Man::Man(int lName){
-    cout << "Constructor is working" << endl;
-    pName = new char[lName + 1 ];
+Man::SetPay(const char* fromBuf){
+    psy = atof(fromBuf + l_name + l_year); // Узнать про atof
 }
 
-Man::Man(int lName){
-    cout << "Constructor is working" << endl;
-    pName = new char[lName + 1 ];
+bool Man::CompareName(const char* name) const {
+    if ((stsrstr(pName, name)) && (pName[strlen(name)] == ' '))
+        return true;
+    else
+        return false;
+}
+
+void Man::Print() const {
+    cout << pName << birth_year << ' ' << pay << endl;
 }
