7fa86e457b81dca6da5d768532ab199027d891a8
ilya-uzun <ilya.uzun@bk.ru>
2020-10-16 08:02:09 +0500

    Task1_1 рабочая

--- C++/Task1_1/Man.cpp
+++ C++/Task1_1/Man.cpp
@@ -14,21 +14,21 @@ Man::~Man(){
     delete [] pName;
 }
 
-Man::SetName(const char* fromBuf){
+void Man::SetName(const char* fromBuf){
     strncpy(pName, fromBuf, l_name);
     pName[l_name] = 0;
 }
 
-Man::SetBirthYear(const char* fromBuf){
+void Man::SetBirthYear(const char* fromBuf){
     birth_year = atoi(fromBuf + l_name); // Узнать про atoi
 }
 
-Man::SetPay(const char* fromBuf){
-    psy = atof(fromBuf + l_name + l_year); // Узнать про atof
+void Man::SetPay(const char* fromBuf){
+    pay = atof(fromBuf + l_name + l_year); // Узнать про atof
 }
 
 bool Man::CompareName(const char* name) const {
-    if ((stsrstr(pName, name)) && (pName[strlen(name)] == ' '))
+    if ((strstr(pName, name)) && (pName[strlen(name)] == ' '))
         return true;
     else
         return false;
