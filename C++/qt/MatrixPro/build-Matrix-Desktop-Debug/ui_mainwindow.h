9728cb27d9fd3bbb8f9fe557fb721ab919555f2f
ilya-uzun <ilya.uzun@bk.ru>
2020-10-19 21:03:19 +0500

    Добавил Libel

--- C++/qt/MatrixPro/build-Matrix-Desktop-Debug/ui_mainwindow.h
+++ C++/qt/MatrixPro/build-Matrix-Desktop-Debug/ui_mainwindow.h
@@ -58,103 +58,123 @@ public:
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         layoutWidget = new QWidget(centralwidget);
         layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
-        layoutWidget->setGeometry(QRect(280, 40, 221, 141));
+        layoutWidget->setGeometry(QRect(460, 40, 224, 224));
         gridLayout_2 = new QGridLayout(layoutWidget);
         gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
         gridLayout_2->setContentsMargins(0, 0, 0, 0);
         textEdit_13 = new QTextEdit(layoutWidget);
         textEdit_13->setObjectName(QString::fromUtf8("textEdit_13"));
+        QFont font;
+        font.setPointSize(12);
+        textEdit_13->setFont(font);
 
         gridLayout_2->addWidget(textEdit_13, 1, 0, 1, 1);
 
         textEdit_14 = new QTextEdit(layoutWidget);
         textEdit_14->setObjectName(QString::fromUtf8("textEdit_14"));
+        textEdit_14->setFont(font);
 
         gridLayout_2->addWidget(textEdit_14, 1, 1, 1, 1);
 
         textEdit_10 = new QTextEdit(layoutWidget);
         textEdit_10->setObjectName(QString::fromUtf8("textEdit_10"));
+        textEdit_10->setFont(font);
 
         gridLayout_2->addWidget(textEdit_10, 0, 0, 1, 1);
 
         textEdit_18 = new QTextEdit(layoutWidget);
         textEdit_18->setObjectName(QString::fromUtf8("textEdit_18"));
+        textEdit_18->setFont(font);
 
         gridLayout_2->addWidget(textEdit_18, 2, 2, 1, 1);
 
         textEdit_15 = new QTextEdit(layoutWidget);
         textEdit_15->setObjectName(QString::fromUtf8("textEdit_15"));
+        textEdit_15->setFont(font);
 
         gridLayout_2->addWidget(textEdit_15, 1, 2, 1, 1);
 
         textEdit_12 = new QTextEdit(layoutWidget);
         textEdit_12->setObjectName(QString::fromUtf8("textEdit_12"));
+        textEdit_12->setFont(font);
 
         gridLayout_2->addWidget(textEdit_12, 0, 2, 1, 1);
 
         textEdit_16 = new QTextEdit(layoutWidget);
         textEdit_16->setObjectName(QString::fromUtf8("textEdit_16"));
+        textEdit_16->setFont(font);
 
         gridLayout_2->addWidget(textEdit_16, 2, 0, 1, 1);
 
         textEdit_11 = new QTextEdit(layoutWidget);
         textEdit_11->setObjectName(QString::fromUtf8("textEdit_11"));
+        textEdit_11->setFont(font);
 
         gridLayout_2->addWidget(textEdit_11, 0, 1, 1, 1);
 
         textEdit_17 = new QTextEdit(layoutWidget);
         textEdit_17->setObjectName(QString::fromUtf8("textEdit_17"));
+        textEdit_17->setFont(font);
 
         gridLayout_2->addWidget(textEdit_17, 2, 1, 1, 1);
 
         widget = new QWidget(centralwidget);
         widget->setObjectName(QString::fromUtf8("widget"));
-        widget->setGeometry(QRect(40, 40, 211, 141));
+        widget->setGeometry(QRect(40, 40, 224, 224));
         gridLayout = new QGridLayout(widget);
         gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
         gridLayout->setContentsMargins(0, 0, 0, 0);
         textEdit = new QTextEdit(widget);
         textEdit->setObjectName(QString::fromUtf8("textEdit"));
+        textEdit->setFont(font);
 
         gridLayout->addWidget(textEdit, 0, 0, 1, 1);
 
         textEdit_2 = new QTextEdit(widget);
         textEdit_2->setObjectName(QString::fromUtf8("textEdit_2"));
+        textEdit_2->setFont(font);
 
         gridLayout->addWidget(textEdit_2, 0, 1, 1, 1);
 
         textEdit_3 = new QTextEdit(widget);
         textEdit_3->setObjectName(QString::fromUtf8("textEdit_3"));
+        textEdit_3->setFont(font);
 
         gridLayout->addWidget(textEdit_3, 0, 2, 1, 1);
 
         textEdit_6 = new QTextEdit(widget);
         textEdit_6->setObjectName(QString::fromUtf8("textEdit_6"));
+        textEdit_6->setFont(font);
 
         gridLayout->addWidget(textEdit_6, 1, 0, 1, 1);
 
         textEdit_5 = new QTextEdit(widget);
         textEdit_5->setObjectName(QString::fromUtf8("textEdit_5"));
+        textEdit_5->setFont(font);
 
         gridLayout->addWidget(textEdit_5, 1, 1, 1, 1);
 
         textEdit_4 = new QTextEdit(widget);
         textEdit_4->setObjectName(QString::fromUtf8("textEdit_4"));
+        textEdit_4->setFont(font);
 
         gridLayout->addWidget(textEdit_4, 1, 2, 1, 1);
 
         textEdit_9 = new QTextEdit(widget);
         textEdit_9->setObjectName(QString::fromUtf8("textEdit_9"));
+        textEdit_9->setFont(font);
 
         gridLayout->addWidget(textEdit_9, 2, 0, 1, 1);
 
         textEdit_8 = new QTextEdit(widget);
         textEdit_8->setObjectName(QString::fromUtf8("textEdit_8"));
+        textEdit_8->setFont(font);
 
         gridLayout->addWidget(textEdit_8, 2, 1, 1, 1);
 
         textEdit_7 = new QTextEdit(widget);
         textEdit_7->setObjectName(QString::fromUtf8("textEdit_7"));
+        textEdit_7->setFont(font);
 
         gridLayout->addWidget(textEdit_7, 2, 2, 1, 1);
 
