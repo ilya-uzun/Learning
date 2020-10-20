0ac9ab85697c3ec34673cc48b968c353fbb283aa
ilya-uzun <ilya.uzun@bk.ru>
2020-10-20 07:59:40 +0500

    Добавил новую таблицу

--- C++/qt/MatrixPro/Matrix/mainwindow.cpp
+++ C++/qt/MatrixPro/Matrix/mainwindow.cpp
@@ -23,9 +23,21 @@ void MainWindow::on_pushButton_clicked()
     int int4  =  ui->lineEdit_4->text().toInt();
     int int5  =  ui->lineEdit_5->text().toInt();
     int int6  =  ui->lineEdit_6->text().toInt();
-    int int7  =  ui->lineEdit_6->text().toInt();
-    int int8  =  ui->lineEdit_6->text().toInt();
-    int int9  =  ui->lineEdit_6->text().toInt();
+    int int7  =  ui->lineEdit_7->text().toInt();
+    int int8  =  ui->lineEdit_8->text().toInt();
+    int int9  =  ui->lineEdit_9->text().toInt();
+
+    //Запись из и перевод из QString в int
+    int int10  =  ui->lineEdit_10->text().toInt();
+    int int11  =  ui->lineEdit_11->text().toInt();
+    int int12  =  ui->lineEdit_12->text().toInt();
+    int int13  =  ui->lineEdit_13->text().toInt();
+    int int14  =  ui->lineEdit_14->text().toInt();
+    int int15  =  ui->lineEdit_15->text().toInt();
+    int int16  =  ui->lineEdit_16->text().toInt();
+    int int17  =  ui->lineEdit_17->text().toInt();
+    int int18  =  ui->lineEdit_18->text().toInt();
+
     //перевод из int  в QString
     QString dec1 = QString::number(int1);
     QString dec2 = QString::number(int2);
--- C++/qt/MatrixPro/build-Matrix-Desktop-Debug/ui_mainwindow.h
+++ C++/qt/MatrixPro/build-Matrix-Desktop-Debug/ui_mainwindow.h
@@ -27,18 +27,18 @@ class Ui_MainWindow
 public:
     QWidget *centralwidget;
     QPushButton *pushButton;
-    QWidget *widget;
+    QWidget *layoutWidget;
     QGridLayout *gridLayout_3;
     QLabel *label;
-    QLabel *label_2;
+    QLabel *label_9;
+    QLabel *label_6;
     QLabel *label_3;
     QLabel *label_5;
-    QLabel *label_6;
-    QLabel *label_8;
-    QLabel *label_9;
-    QLabel *label_4;
     QLabel *label_7;
-    QWidget *widget1;
+    QLabel *label_8;
+    QLabel *label_4;
+    QLabel *label_2;
+    QWidget *layoutWidget1;
     QGridLayout *gridLayout;
     QLineEdit *lineEdit;
     QLineEdit *lineEdit_2;
@@ -49,6 +49,17 @@ public:
     QLineEdit *lineEdit_7;
     QLineEdit *lineEdit_8;
     QLineEdit *lineEdit_9;
+    QWidget *layoutWidget_2;
+    QGridLayout *gridLayout_2;
+    QLineEdit *lineEdit_10;
+    QLineEdit *lineEdit_11;
+    QLineEdit *lineEdit_12;
+    QLineEdit *lineEdit_13;
+    QLineEdit *lineEdit_14;
+    QLineEdit *lineEdit_15;
+    QLineEdit *lineEdit_16;
+    QLineEdit *lineEdit_17;
+    QLineEdit *lineEdit_18;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -61,65 +72,65 @@ public:
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(360, 250, 111, 61));
-        widget = new QWidget(centralwidget);
-        widget->setObjectName(QString::fromUtf8("widget"));
-        widget->setGeometry(QRect(50, 330, 221, 191));
-        gridLayout_3 = new QGridLayout(widget);
+        pushButton->setGeometry(QRect(60, 290, 111, 61));
+        layoutWidget = new QWidget(centralwidget);
+        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
+        layoutWidget->setGeometry(QRect(60, 370, 221, 191));
+        gridLayout_3 = new QGridLayout(layoutWidget);
         gridLayout_3->setObjectName(QString::fromUtf8("gridLayout_3"));
         gridLayout_3->setContentsMargins(0, 0, 0, 0);
-        label = new QLabel(widget);
+        label = new QLabel(layoutWidget);
         label->setObjectName(QString::fromUtf8("label"));
 
         gridLayout_3->addWidget(label, 0, 0, 1, 1);
 
-        label_2 = new QLabel(widget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-
-        gridLayout_3->addWidget(label_2, 0, 1, 1, 1);
-
-        label_3 = new QLabel(widget);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
-
-        gridLayout_3->addWidget(label_3, 0, 2, 1, 1);
-
-        label_5 = new QLabel(widget);
-        label_5->setObjectName(QString::fromUtf8("label_5"));
-
-        gridLayout_3->addWidget(label_5, 1, 1, 1, 1);
-
-        label_6 = new QLabel(widget);
-        label_6->setObjectName(QString::fromUtf8("label_6"));
-
-        gridLayout_3->addWidget(label_6, 1, 2, 1, 1);
-
-        label_8 = new QLabel(widget);
-        label_8->setObjectName(QString::fromUtf8("label_8"));
-
-        gridLayout_3->addWidget(label_8, 2, 1, 1, 1);
-
-        label_9 = new QLabel(widget);
+        label_9 = new QLabel(layoutWidget);
         label_9->setObjectName(QString::fromUtf8("label_9"));
 
         gridLayout_3->addWidget(label_9, 2, 2, 1, 1);
 
-        label_4 = new QLabel(widget);
-        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_6 = new QLabel(layoutWidget);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
 
-        gridLayout_3->addWidget(label_4, 1, 0, 1, 1);
+        gridLayout_3->addWidget(label_6, 1, 2, 1, 1);
 
-        label_7 = new QLabel(widget);
+        label_3 = new QLabel(layoutWidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        gridLayout_3->addWidget(label_3, 0, 2, 1, 1);
+
+        label_5 = new QLabel(layoutWidget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+
+        gridLayout_3->addWidget(label_5, 1, 1, 1, 1);
+
+        label_7 = new QLabel(layoutWidget);
         label_7->setObjectName(QString::fromUtf8("label_7"));
 
         gridLayout_3->addWidget(label_7, 2, 0, 1, 1);
 
-        widget1 = new QWidget(centralwidget);
-        widget1->setObjectName(QString::fromUtf8("widget1"));
-        widget1->setGeometry(QRect(50, 60, 221, 211));
-        gridLayout = new QGridLayout(widget1);
+        label_8 = new QLabel(layoutWidget);
+        label_8->setObjectName(QString::fromUtf8("label_8"));
+
+        gridLayout_3->addWidget(label_8, 2, 1, 1, 1);
+
+        label_4 = new QLabel(layoutWidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        gridLayout_3->addWidget(label_4, 1, 0, 1, 1);
+
+        label_2 = new QLabel(layoutWidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        gridLayout_3->addWidget(label_2, 0, 1, 1, 1);
+
+        layoutWidget1 = new QWidget(centralwidget);
+        layoutWidget1->setObjectName(QString::fromUtf8("layoutWidget1"));
+        layoutWidget1->setGeometry(QRect(60, 60, 221, 211));
+        gridLayout = new QGridLayout(layoutWidget1);
         gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
         gridLayout->setContentsMargins(0, 0, 0, 0);
-        lineEdit = new QLineEdit(widget1);
+        lineEdit = new QLineEdit(layoutWidget1);
         lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
         QFont font;
         font.setPointSize(12);
@@ -128,53 +139,113 @@ public:
 
         gridLayout->addWidget(lineEdit, 0, 0, 1, 1);
 
-        lineEdit_2 = new QLineEdit(widget1);
+        lineEdit_2 = new QLineEdit(layoutWidget1);
         lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
         lineEdit_2->setFont(font);
 
         gridLayout->addWidget(lineEdit_2, 0, 1, 1, 1);
 
-        lineEdit_3 = new QLineEdit(widget1);
+        lineEdit_3 = new QLineEdit(layoutWidget1);
         lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
         lineEdit_3->setFont(font);
 
         gridLayout->addWidget(lineEdit_3, 0, 2, 1, 1);
 
-        lineEdit_5 = new QLineEdit(widget1);
+        lineEdit_5 = new QLineEdit(layoutWidget1);
         lineEdit_5->setObjectName(QString::fromUtf8("lineEdit_5"));
         lineEdit_5->setFont(font);
 
         gridLayout->addWidget(lineEdit_5, 1, 0, 1, 1);
 
-        lineEdit_4 = new QLineEdit(widget1);
+        lineEdit_4 = new QLineEdit(layoutWidget1);
         lineEdit_4->setObjectName(QString::fromUtf8("lineEdit_4"));
         lineEdit_4->setFont(font);
 
         gridLayout->addWidget(lineEdit_4, 1, 1, 1, 1);
 
-        lineEdit_6 = new QLineEdit(widget1);
+        lineEdit_6 = new QLineEdit(layoutWidget1);
         lineEdit_6->setObjectName(QString::fromUtf8("lineEdit_6"));
         lineEdit_6->setFont(font);
 
         gridLayout->addWidget(lineEdit_6, 1, 2, 1, 1);
 
-        lineEdit_7 = new QLineEdit(widget1);
+        lineEdit_7 = new QLineEdit(layoutWidget1);
         lineEdit_7->setObjectName(QString::fromUtf8("lineEdit_7"));
         lineEdit_7->setFont(font);
 
         gridLayout->addWidget(lineEdit_7, 2, 0, 1, 1);
 
-        lineEdit_8 = new QLineEdit(widget1);
+        lineEdit_8 = new QLineEdit(layoutWidget1);
         lineEdit_8->setObjectName(QString::fromUtf8("lineEdit_8"));
         lineEdit_8->setFont(font);
 
         gridLayout->addWidget(lineEdit_8, 2, 1, 1, 1);
 
-        lineEdit_9 = new QLineEdit(widget1);
+        lineEdit_9 = new QLineEdit(layoutWidget1);
         lineEdit_9->setObjectName(QString::fromUtf8("lineEdit_9"));
         lineEdit_9->setFont(font);
 
         gridLayout->addWidget(lineEdit_9, 2, 2, 1, 1);
+
+        layoutWidget_2 = new QWidget(centralwidget);
+        layoutWidget_2->setObjectName(QString::fromUtf8("layoutWidget_2"));
+        layoutWidget_2->setGeometry(QRect(340, 60, 221, 211));
+        gridLayout_2 = new QGridLayout(layoutWidget_2);
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        gridLayout_2->setContentsMargins(0, 0, 0, 0);
+        lineEdit_10 = new QLineEdit(layoutWidget_2);
+        lineEdit_10->setObjectName(QString::fromUtf8("lineEdit_10"));
+        lineEdit_10->setFont(font);
+
+        gridLayout_2->addWidget(lineEdit_10, 0, 0, 1, 1);
+
+        lineEdit_11 = new QLineEdit(layoutWidget_2);
+        lineEdit_11->setObjectName(QString::fromUtf8("lineEdit_11"));
+        lineEdit_11->setFont(font);
+
+        gridLayout_2->addWidget(lineEdit_11, 0, 1, 1, 1);
+
+        lineEdit_12 = new QLineEdit(layoutWidget_2);
+        lineEdit_12->setObjectName(QString::fromUtf8("lineEdit_12"));
+        lineEdit_12->setFont(font);
+
+        gridLayout_2->addWidget(lineEdit_12, 0, 2, 1, 1);
+
+        lineEdit_13 = new QLineEdit(layoutWidget_2);
+        lineEdit_13->setObjectName(QString::fromUtf8("lineEdit_13"));
+        lineEdit_13->setFont(font);
+
+        gridLayout_2->addWidget(lineEdit_13, 1, 0, 1, 1);
+
+        lineEdit_14 = new QLineEdit(layoutWidget_2);
+        lineEdit_14->setObjectName(QString::fromUtf8("lineEdit_14"));
+        lineEdit_14->setFont(font);
+
+        gridLayout_2->addWidget(lineEdit_14, 1, 1, 1, 1);
+
+        lineEdit_15 = new QLineEdit(layoutWidget_2);
+        lineEdit_15->setObjectName(QString::fromUtf8("lineEdit_15"));
+        lineEdit_15->setFont(font);
+
+        gridLayout_2->addWidget(lineEdit_15, 1, 2, 1, 1);
+
+        lineEdit_16 = new QLineEdit(layoutWidget_2);
+        lineEdit_16->setObjectName(QString::fromUtf8("lineEdit_16"));
+        lineEdit_16->setFont(font);
+
+        gridLayout_2->addWidget(lineEdit_16, 2, 0, 1, 1);
+
+        lineEdit_17 = new QLineEdit(layoutWidget_2);
+        lineEdit_17->setObjectName(QString::fromUtf8("lineEdit_17"));
+        lineEdit_17->setFont(font);
+
+        gridLayout_2->addWidget(lineEdit_17, 2, 1, 1, 1);
+
+        lineEdit_18 = new QLineEdit(layoutWidget_2);
+        lineEdit_18->setObjectName(QString::fromUtf8("lineEdit_18"));
+        lineEdit_18->setFont(font);
+
+        gridLayout_2->addWidget(lineEdit_18, 2, 2, 1, 1);
 
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
@@ -195,14 +266,14 @@ public:
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
         pushButton->setText(QApplication::translate("MainWindow", "PushButton", nullptr));
         label->setText(QString());
-        label_2->setText(QString());
+        label_9->setText(QString());
+        label_6->setText(QString());
         label_3->setText(QString());
         label_5->setText(QString());
-        label_6->setText(QString());
-        label_8->setText(QString());
-        label_9->setText(QString());
-        label_4->setText(QString());
         label_7->setText(QString());
+        label_8->setText(QString());
+        label_4->setText(QString());
+        label_2->setText(QString());
     } // retranslateUi
 
 };
