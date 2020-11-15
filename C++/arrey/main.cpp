791bf5ccb773fa73b26553a37cbf0fead9c3b8ab
ilya-uzun <ilya.uzun@bk.ru>
2020-11-15 21:45:28 +0500

    пока не получается записать в середину массива

--- C++/arrey/main.cpp
+++ C++/arrey/main.cpp
@@ -24,20 +24,32 @@ void ShowArray(int* const arr, const int size)
     cout << endl;
 }
 
-void push_back(int *&arr, int &size, int value)
+void push_back(int *&arr, int &size, int value, int nn)
 {
     // добавлениев массив
     // value - последий элемент массива или новой значение которое будем добавлять
     // int &size - размер массива, передача по ссылке & для того, что бы вернуть новое значение переменной
     // int *&arr - даёт возможность подменить адрес, что бы он указывал на новый массив 
+    size++;
+    int *newArr = new int[size+1]; // сoздаём новый массив больший на единицу
     
-    int *newArr = new int[size+1]; // свздаём новый массив больший на единицу
     for (int i = 0; i < size; i++)
     {
-        newArr[i] = arr[i];// в каждый новый элемент вкладываем  элементы предыдущего
+        if (nn < size) 
+        {
+        newArr[i] = arr[i];// в каждый новый элемент вкладываем  элементы предыдущего     
+        } else  if (nn > size)
+        {
+            newArr[nn] = 100;
+        } 
+        // else
+        // {
+        //     newArr[i] = arr[i];
+        // }
+        
     }
-    newArr[size] = value; // добовление в тоследнюю ячейку
-    size++; // умеличиваем размер на единицу
+    //newArr[size] = value; // добавление в последнюю ячейку
+     // умеличиваем размер на единицу
 
     delete[] arr; // удаляем старый массив
 
@@ -48,7 +60,7 @@ void pop_back(int *&arr, int &size)
 {
     // удаляем элемент из  массива
     size--; // уменьшаем размер на единицу
-    int *newArr = new int[size]; // свздаём новый массив больший на единицу
+    int *newArr = new int[size]; // создаём новый массив больший на единицу
     for (int i = 0; i < size; i++)
     {
         newArr[i] = arr[i];// в каждый новый элемент вкладываем  элементы предыдущего
@@ -65,10 +77,10 @@ int main()
     int *arr = new int[size]; // указатель под массив и выделяем под него память
     FillArrey(arr, size); // вызываем функцию для создания массива
     ShowArray(arr, size); // вывод на экран
-    push_back(arr, size, 6);
+    push_back(arr, size, 6, 2);
     ShowArray(arr, size); // вывод на экран новый массив
-    pop_back(arr, size);
-    ShowArray(arr, size); // вывод на экран новый массив
+    //pop_back(arr, size);
+    //ShowArray(arr, size); // вывод на экран новый массив
     delete[] arr;         
    
   return 0;
