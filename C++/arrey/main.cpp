df2d530878ea5481c462aa661f90a2f74c204901
ilya-uzun <ilya.uzun@bk.ru>
2020-11-16 16:46:37 +0500

    lab4_7 без функций проблема при добавлении 100

--- C++/arrey/main.cpp
+++ C++/arrey/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 void FillArrey(int* const arr, const int size)
 {
     // Создаём рандомный массив с числами от 0 до 10
+    srand(time(0)); //позволяет запускать программу несколько раз, иначе одинаковые значения
     for (int i = 0; i < size; i++)
     {
         arr[i] = rand() % 10; 
@@ -26,22 +27,29 @@ void ShowArray(int* const arr, const int size)
 
 void push_back(int *&arr, int &size, int value)
 {
-
     size++;
     int *newArr = new int[size+1]; // сoздаём новый массив больший на единицу
-    // Присваеваем элементы к новому масству
-    for (int i = 0; i < size; i++)
-    {
-        newArr[i] = arr[i];
-    }
     
-    // добавляем 100 после выбраной позиции value
-    for (int i = size; i >= value-1; i--)
+     if (value > 0 && value < size)// проверка правильности ввода
     {
-        newArr[i+1] = arr[i];
-        newArr[value-1] = 100;
-    }
+        // Присваеваем элементы к новому масству
+        for (int i = 0; i < size; i++)
+        {
+            newArr[i] = arr[i];
+        }
     
+        // добавляем 100 после выбраной позиции value
+        for (int i = size; i >= value-1; i--)
+            {
+                newArr[i+1] = arr[i];
+                newArr[value-1] = 100;// добовляем значени 100
+            }
+    }else
+        {
+             cout <<  "Введенно не корретное значение позиции элемента" << endl;
+             cout <<  "Введите значение с 1 по последеий элемент " << endl;
+        }
+        
     delete[] arr; // удаляем старый массив
 
     arr = newArr; // изменяем адрес, так как элемен передан по ссылке, то что мы с ним сделаем повлияет а указатель arr
@@ -64,7 +72,7 @@ void pop_back(int *&arr, int &size)
 
 int main()
 {
-    int size = 5;
+    int size = 100;
     int *arr = new int[size]; // указатель под массив и выделяем под него память
     FillArrey(arr, size); // вызываем функцию для создания массива
     ShowArray(arr, size); // вывод на экран
