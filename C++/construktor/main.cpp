36879af7573f023157e17ad244db474a1bcd6a39
ilya-uzun <ilya.uzun@bk.ru>
2020-12-18 17:31:47 +0500

    конструктор копирования

--- C++/construktor/main.cpp
+++ C++/construktor/main.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 
 using namespace std;
-class Pointer {
-  Pointer (){cout << "вызов конструктора объектов"<<this << endl;}
-  Pointer(int px, int py) : x(px), y(py){
+
+class Point{
+public:
+  Point(const Point& other){
+      cout <<"Вызоа конструктора копирования"<<this << endl;
+      this->x = other.x;
+      this->y = other.y;
+  }
+  Point(){cout << "вызов конструктора объектов"<<this << endl;}
+  Point(int px, int py) : x(px), y(py){
     cout << "Вызов конструктора объектов с 2-мя аругментами"<< this << endl;
   }
-  ~Pointer() {cout "Вызов деструктора объектов"<<this << endl;}
+  ~Point() {cout << "Вызов деструктора объектов"<<this << endl;}
 
 private:
-    int x? y;
+    int x, y;
 
 public:
     void setCoprd(int x, int y){
       this->x = x; 
       this->y = y;
       }
+};
+
+void foo(Point pt){
+    cout << "Вывод функции foo" << endl;
 }
+
+
 int main ()
 {
-  Pointer a(2, 3), b; // где при а(2,3)вызов конструктора с аргумнтами, а b - вызов конструктора по умолчанию
-}
+  Point a(1,2), b; // где при а(2,3)вызов конструктора с аргумнтами, а b - вызов конструктора по умолчанию
+  foo(a);
+};
