d9c3af917af9134b660ba14cfbffa3b6e149723f
ilya-uzun <ilya.uzun@bk.ru>
2020-12-19 16:16:38 +0500

    закончил с конструкторами

--- C++/construktor/main.cpp
+++ C++/construktor/main.cpp
@@ -9,7 +9,10 @@ public:
       this->x = other.x;
       this->y = other.y;
   }
-  Point(){cout << "вызов конструктора объектов"<<this << endl;}
+  Point(){
+      cout << "вызов конструктора объектов"<<this << endl;
+      int_static++;
+  }
   Point(int px, int py) : x(px), y(py){
     cout << "Вызов конструктора объектов с 2-мя аругментами"<< this << endl;
   }
@@ -17,8 +20,9 @@ public:
 
 private:
     int x, y;
-
+    
 public:
+    static int int_static;
     void setCoprd(int x, int y){
       this->x = x; 
       this->y = y;
@@ -29,9 +33,11 @@ void foo(Point pt){
     cout << "Вывод функции foo" << endl;
 }
 
+int Point::int_static = 0; //обязательно нужно иницыализировать 
 
 int main ()
 {
   Point a(1,2), b; // где при а(2,3)вызов конструктора с аргумнтами, а b - вызов конструктора по умолчанию
   foo(a);
+  cout << Point::int_static << endl;
 };
