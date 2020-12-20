d88e5ceeb4ee96f28385db00434e583fda454f4b
ilya-uzun <ilya.uzun@bk.ru>
2020-12-20 08:59:07 +0500

    работает

--- C++/var_7/lab_5.cpp
+++ C++/var_7/lab_5.cpp
@@ -1,101 +1,84 @@
 #include <iostream>
 #include <iomanip>
-
-#include <time.h>
+#include <time.h>// генератор случайных чисел
 using namespace std;
 
-
-void FillArrey(int **matrix_1, int nR, int nC)
+void FillArrey(int **matrix, int nR, int nC)
 {
     // Создаём рандомный массив с числами от 0 до 5, что-бы чаще совпадали первые элементы 
 	srand(time(0)); //позволяет запускать программу несколько раз, иначе одинаковые значения
-   	for (int  i= 0; i < nR; i++)
+   	for (int  i= 0; i < nR; ++i)
 	 {
-		for (int j = 0; j < nC; j++)
+		for (int j = 0; j < nC; ++j)
 		{
-			matrix_1[i][j] = rand() % 5;	
+			matrix[i][j] = rand() % 5;	
 		}
 	 }
 }
 
-void ShowArray(int **matrix_1, int nR, int nC)
+void ShowArray(int **matrix, int nR, int nC)
 {
     // Выводим массив
-	for (int  i= 0; i < nR; i++)
+	for (int  i= 0; i < nR; ++i)
 	 {
-		for (int j = 0; j < nC; j++)
+		for (int j = 0; j < nC; ++j)
 		{
-			cout << matrix_1[i][j] << " ";
+			cout << matrix[i][j] << " ";
 		}
 		cout << endl;
-	 }  
+	 } 
 }
-void ChangeArrey(int **matrix_1, int nR, int nC)
+void ChangeArrey(int **matrix, int nR, int nC)
   {
 
       int  x=0, y=1, temp;
  
+ 
+     // Выводим массив
+	for (int  i= 0; i < nR; ++i)
+	 {
+		for (int j = 0; j < nC; ++j)
+		{
+			if (matrix[0][0]==matrix[0][1]) {x=0; y=1;}
+			else if (matrix[0][0]==matrix[0][2]) {x=0; y=2;}
+			else if (matrix[0][0]==matrix[0][3]) {x=0; y=3;}
+			else if (matrix[0][0]==matrix[0][4]) {x=0; y=4;}
+			else if (matrix[0][1]==matrix[0][2]) {x=1; y=2;}
+			else if (matrix[0][1]==matrix[0][3]) {x=1; y=3;}
+			else if (matrix[0][1]==matrix[0][4]) {x=1; y=4;}
+			else if (matrix[0][2]==matrix[0][3]) {x=2; y=3;}
+			else if (matrix[0][3]==matrix[0][4]) {x=3; y=4;}			
+		}
+	 } 
       
     // Выводим массив
-	for (int  i= 0; i < nR; i++)
+	for (int  i= 0; i < nR; ++i)
 	 {
-	    temp = matrix_1[i][x];
-        matrix_1[i][x] = matrix_1[i][y];
-        matrix_1[i][y] = temp;
+	    temp = matrix[i][x];
+        matrix[i][x] = matrix[i][y];
+        matrix[i][y] = temp;
 		cout << endl;    
 	 } 
-
   }
-
 
 int main()
 {
 	const int nRow = 5;// строки
 	const int nCol = 5;// столбцы
-	int **matrix_1= new int* [nRow];// указатель на указатель типа int, память под массив указателей на строки массива
-	int i,j;
+	int **matrix= new int* [nRow];// указатель на указатель типа int, память под массив указателей на строки массива
+
 	// Цикл для выделение памяти под каждую строку
-	for (i=0; i<nRow; i++)
+	for (int i=0; i < nRow; ++i)
 	  {
-		matrix_1[i] = new int[nCol]; // каждому элементу указателей на сроку присваиваим адрес начала участка памяти
+		matrix[i] = new int[nCol]; // каждому элементу указателей на сроку присваиваим адрес начала участка памяти
 	  }
 
-	FillArrey(matrix_1, nRow, nCol); // вызываем функцию для создания массива
-    ShowArray(matrix_1, nRow, nCol); // вывод на экран
-	ChangeArrey(matrix_1, nRow, nCol); // изменяем массив
-    ShowArray(matrix_1, nRow, nCol); // вывод на экран
-	delete[] matrix_1;
+	FillArrey(matrix, nRow, nCol); // вызываем функцию для создания массива
+    ShowArray(matrix, nRow, nCol); // вывод на экран
+	ChangeArrey(matrix, nRow, nCol); // изменяем массив
+    ShowArray(matrix, nRow, nCol); // вывод на экран
+	delete[] matrix;
 	return 0;
 }
 
 
-/* вроде рабочий код
-
-const int n = 3;
-const int m = 3;
-int mass[n][m];
-for (int i = 0; i < n; i++)
-{
-    for (int j = 0; j < m; j++)
-    {
-        mass[i][j] = rand() % 5;
-        std::cout << mass[i][j] << " ";
-    }
-    std::cout << std::endl;
-}
-
-for (int i = 0; i < n; i++)
-{
-    for (int j = 0; j < m; j++)
-    {
-        for (int s = i; s < n; s++)
-        {
-            for (int t = j + 1; t < m; t++)
-                if (mass[i][j] == mass[s][t])
-                {
-                    std::cout << "mass[" << i << "][ " << j <<
-                        "] == mass[ " << s << "][" << t << "]" << std::endl;
-                }
-        }
-    }
-*/
