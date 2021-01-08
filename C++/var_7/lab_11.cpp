e966fd5290bfec7f78b4edd24686a63fe74ce510
ilya-uzun <ilya.uzun@bk.ru>
2021-01-08 15:52:07 +0500

    лаб 11 сделал красивее

--- C++/var_7/lab_11.cpp
+++ C++/var_7/lab_11.cpp
@@ -1,4 +1,8 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdarg>
+
 using namespace std;
 
 class MyList {                                       //Список MyList
@@ -6,97 +10,102 @@ class MyList {                                       //Список MyList
     MyList* Head, * Tail, * Next, * Prev;
 public:
     MyList() :count_(0), Head(NULL), Tail(NULL) {};     //Инициализация с помощью конструктора по умолчанию
-    void Add(int);
-    void Show();
-    void Del(int);                                  //Функция принимает целочисленный параметр, обозначающий номер удаляемого элемента
-    ~MyList();
-};
-
-/*ДОБАВЛЕНИЕ ЭЛЕМЕНТА В СПИСОК*/
-void MyList::Add(int x) {
-    MyList* temp = new MyList;
-    temp->x = x;
-    temp->Next = NULL;
-    count_++;
-    if (!Head) {
-        temp->Prev = NULL;
-        Head = temp;
-        Tail = Head;
+    /*ДОБАВЛЕНИЕ ЭЛЕМЕНТА В СПИСОК*/
+    void Add(int x){
+        MyList* temp = new MyList;
+        temp->x = x;
+        temp->Next = NULL;
+        count_++;
+        if (!Head) {
+            temp->Prev = NULL;
+            Head = temp;
+            Tail = Head;
+        }
+        else {
+            temp->Prev = Tail;                  
+            Tail->Next = temp;                
+            Tail = temp;                        
+        }
     }
-    else {
-        temp->Prev = Tail;                  
-        Tail->Next = temp;                
-        Tail = temp;                        
+    /*ПОКАЗЫВАЕТ СПИСОК НА ЭКРАНЕ*/
+    void Show(){
+        MyList* t = Head;
+        while (t) {
+            cout << t->x << " ";
+            t = t->Next;
+        }
+        cout << "\n\n";
     }
-}
+    /*ФУНКЦИЯ УДАЛЕНИЯ  ЭЛЕМЕНТА ДВУСВЯЗНОГО СПИСКА*/
+    void Del(int x){ //Функция принимает целочисленный параметр, обозначающий номер удаляемого элемента
+     //удаляем первый элемент
+        if ((x == 1) and (Head->Next)) {                     
+            MyList* temp = Head;	                        //Указываем, что нам нужно начало списка
+            Head = Head->Next;	                            
+            Head->Prev = NULL;	                            //Делаем так, чтоб предыдущий началу элемент был пустым
+            delete temp;		                            //Удаляем удаляемое начало
+            count_--;		                                //Обязательно уменьшаем счетчик
+            return;		                                //И выходим из функции
+        }
+        else if ((x == 1) and (Head == Tail)) {           
 
-/*ПОКАЗЫВАЕТ СПИСОК НА ЭКРАНЕ*/
-void MyList::Show() {
-    MyList* t = Head;
-    while (t) {
-        cout << t->x << " ";
-        t = t->Next;
+            Head->Next = NULL;	                            //обнуляем все что нужно
+            Head = NULL;
+            delete Head;		                            //Удаляем указатель на начало
+            count_ = 0;		                                //обозначаем, что в списке ноль элементов
+            return;			                                //выходим из функции
+        }
+
+        // удаляемый элемент является последним элементом списка
+         if (x == count_) {
+            MyList* temp = Tail;	                            //Указываем, что нам нужен конец
+            Tail = Tail->Prev;	                                
+            Tail->Next = NULL;	                                //Обозначаем, что впереди  пусто
+            delete temp;	                                    //Очищаем память 
+            count_--;		                                    //Обязательно уменьшаем счетчик элементов
+            return;		                                    //И выходим из функции
+            }
+
+        //удаляемый элемент лежит где-то в середине списка
+
+        MyList* temp = Head, * temp2;                        //temp-Удаляемый элемент, temp2 нужен, чтобы не потерять данные
+
+        cout<<count_<<"\n";
+        for (int i = 0; i < x - 1; i++) temp = temp->Next;  //Идем к адресу удаляемого элемента
+
+        temp2 = temp;	                                //Временно запоминаем адрес удаляемого элемента
+        temp2->Prev->Next = temp->Next;	            //Записываем данные, что следующий за перед сейчас удаляемым элементом - это следующий от удаляемого
+        temp2->Next->Prev = temp->Prev;               //а предыдущий для следующего - это предыдущий для удаляемого
+        delete temp;                               //теперь смело можно освободить память, удалив адрес на начало удаляемого элемента
+        count_--;                                         //Обязательно уменьшаем число элементов в списке.
     }
-    cout << "\n\n";
-}
+    
+    
+    void WriteFile (const char* filename) {
+            ofstream fout(filename, ios::out);
+            MyList* temp = Head;
+            
+            if (!fout.is_open()){
+                cout << "Не удалось открыть файл." << endl;
+            return;
+            }
 
-
-/*ФУНКЦИЯ УДАЛЕНИЯ  ЭЛЕМЕНТА ДВУСВЯЗНОГО СПИСКА*/
-void MyList::Del(int x) {
-    //удаляем первый элемент
-   
-    if ((x == 1) and (Head->Next)) {                     
-        MyList* temp = Head;	                        //Указываем, что нам нужно начало списка
-        Head = Head->Next;	                            
-        Head->Prev = NULL;	                            //Делаем так, чтоб предыдущий началу элемент был пустым
-        delete temp;		                            //Удаляем удаляемое начало
-        count_--;		                                //Обязательно уменьшаем счетчик
-        return;		                                //И выходим из функции
-    }
-    else if ((x == 1) and (Head == Tail)) {           
-
-        Head->Next = NULL;	                            //обнуляем все что нужно
-        Head = NULL;
-        delete Head;		                            //Удаляем указатель на начало
-        count_ = 0;		                                //обозначаем, что в списке ноль элементов
-        return;			                                //выходим из функции
-    }
-
-    // удаляемый элемент является последним элементом списка
-   if (x == count_) {
-        MyList* temp = Tail;	                            //Указываем, что нам нужен конец
-        Tail = Tail->Prev;	                                
-        Tail->Next = NULL;	                                //Обозначаем, что впереди  пусто
-        delete temp;	                                    //Очищаем память 
-        count_--;		                                    //Обязательно уменьшаем счетчик элементов
-        return;		                                    //И выходим из функции
-    }
-
-    //удаляемый элемент лежит где-то в середине списка
-
-    MyList* temp = Head, * temp2;                        //temp-Удаляемый элемент, temp2 нужен, чтобы не потерять данные
-
-    cout<<count_<<"\n";
-    for (int i = 0; i < x - 1; i++) temp = temp->Next;  //Идем к адресу удаляемого элемента
-
-    temp2 = temp;	                                //Временно запоминаем адрес удаляемого элемента
-    temp2->Prev->Next = temp->Next;	            //Записываем данные, что следующий за перед сейчас удаляемым элементом - это следующий от удаляемого
-    temp2->Next->Prev = temp->Prev;               //а предыдущий для следующего - это предыдущий для удаляемого
-    delete temp;                               //теперь смело можно освободить память, удалив адрес на начало удаляемого элемента
-    count_--;                                         //Обязательно уменьшаем число элементов в списке.
-    }
-
-
-/*ДЕСТРУКТОР ДЛЯ КОРРЕКТНОГО ВЫСВОБОЖДЕНИЯ ПАМЯТИ*/
-MyList::~MyList() {
-    while (Head) {
+            while (temp != nullptr)
+            {
+                fout << temp->x  << endl;
+                temp = temp->Next;
+            } 
+                fout.close();
+        } 
+    /*ДЕСТРУКТОР ДЛЯ КОРРЕКТНОГО ВЫСВОБОЖДЕНИЯ ПАМЯТИ*/                               
+    ~MyList(){
+        while (Head) {
         Tail = Head->Next;
         delete Head;
         Head = Tail;
+        }
     }
-}
-
-
+};
 
 int main() {
     MyList List1;
@@ -110,21 +119,11 @@ int main() {
     List1.Show();
     List1.Add(1000);// обозначаем какой элемент добавляем в конец
     List1.Show();
- 
-    // const char filename[15] = "myList.txt";        // создаем переменую хронящею 
-	// cout << "Записываем  в файл..." << endl;
-	// WriteFile(List, filename);
-	// cout << "Удаляем список..." << endl;
-	// listDelete(List);
-	// listPrint(List);
-	// l_list* restoredList = listCreate();
-	// cout << "Восстанавливаем из файла..." << endl;
-	// listRead(restoredList, filename);
-	// cout << "Список содержит:" << endl;
-	// listPrint(restoredList);
-	// cout << "Удаляем ..." << endl;
-	// listDelete(restoredList);
-	// listPrint(List);
+
+    const char filename[15] = "myList.txt";        // создаем переменую хронящею 
+	cout << "Записываем  в файл..." << endl;
+    List1.WriteFile(filename);
+  
 }
 
 //     1. Создание списка.
@@ -156,7 +155,3 @@ int main() {
 // Сформировать двунаправленный список. 
 // Удалить из него первый элемент, добавить элемент в конец списка.
 
-// вар 9
-// записи в линейном списке содержат ключевое поле типа int. 
-// Сформировать двунаправленный список. Удалить из него К элементов перед элементом с заданным номером, 
-// добавить К элементов в конец списка.
