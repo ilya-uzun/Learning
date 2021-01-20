ac59c23574a7480c9061f090e8c7671e6194878b
ilya-uzun <ilya.uzun@bk.ru>
2021-01-20 21:34:17 +0500

    тест js

---
+++ js/hello.js
@@ -0,0 +1,13 @@
+// переменные
+let message, age = 32;
+message = 'Илья';
+console.log('Привет', message, age);
+
+// таймер
+setTimeout(() => {
+    console.log('Прошло 5000 мс')
+}, 5000);
+
+setTimeout(() => {
+    console.log('Прошло 1000 мс')
+}, 1000);
