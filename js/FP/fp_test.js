cced6dfcbcde722fe414fd228f21340e5ffa8828
ilya-uzun <ilya.uzun@bk.ru>
2021-04-17 23:04:11 +0500

    Update fp_test.js

--- js/FP/fp_test.js
+++ js/FP/fp_test.js
@@ -1,7 +1,20 @@
-
-
-    function half(x){
-        return x+2;
+//функция первого порядка - это функция которая ведет себя как переменная
+const main = function (a, b){
+    return a + b;
+}
+// Функция высшего порядка
+function mainVP(a){
+    return function(b){
+        return a*b;
     }
+}
 
+function half(x){
+    return x+2;
+}
+let func = mainVP(2);
+let res = func(3);
+
+console.log(res);
+console.log(main);
 console.log(half(2));
