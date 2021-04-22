c6189b55a9020d6acb851ecb4013ce42c2bd2ece
ilya-uzun <ilya.uzun@bk.ru>
2021-04-22 20:04:24 +0500

    Update kotlin

--- kotlin/typein.kt
+++ kotlin/typein.kt
@@ -1,4 +1,11 @@
 fun main(){
    var ex: Int = 5
    println(ex)
+
+   val race = 1
+   val frac = when (race) {
+      1 -> println("One")
+      2 -> println("Two")
+      else -> println("exit")
+      }
    }
