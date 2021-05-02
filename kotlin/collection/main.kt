fc861794c67e35df1eac0b31c665e4adc51c306e
ilya-uzun <ilya.uzun@bk.ru>
2021-05-02 22:00:57 +0500

    урок по коллекциям

---
+++ kotlin/collection/main.kt
@@ -0,0 +1,10 @@
+package com.kiparo
+
+fun main(args: Array<String>){
+    println("ddd")
+    // Колекции 
+    val list = ListOf("111", "222", "3333")
+    val text = list.get(0)
+    println("Text = $text")
+
+}
