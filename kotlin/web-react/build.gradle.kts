b94888cb7be122c6fd1ef3e3fc48b98045fccc19
ilya-uzun <ilya.uzun@bk.ru>
2021-05-05 22:18:17 +0500

    Шафбон котлин веб

---
+++ kotlin/web-react/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("org.jetbrains.kotlin.js") version "1.4.31"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
+    mavenCentral()
+    jcenter()
+}
+
+kotlin {
+    js {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+        binaries.executable()
+    }
+}
+
+// Блок зависимостей dependencies содержит все нужные для практики внешние библиотеки:
+
+dependencies {
+
+    //React, React DOM + Wrappers (chapter 3)
+    implementation("org.jetbrains:kotlin-react:17.0.1-pre.148-kotlin-1.4.21")
+    implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.148-kotlin-1.4.21")
+    implementation(npm("react", "17.0.1"))
+    implementation(npm("react-dom", "17.0.1"))
+
+    //Kotlin Styled (chapter 3)
+    implementation("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.21")
+    implementation(npm("styled-components", "~5.2.1"))
+
+    //Video Player (chapter 7)
+    implementation(npm("react-youtube-lite", "1.0.1"))
+
+    //Share Buttons (chapter 7)
+    implementation(npm("react-share", "~4.2.1"))
+
+    //Coroutines (chapter 8)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+}
+
+// Heroku Deployment (chapter 9)
+tasks.register("stage") {
+    dependsOn("build")
+}
---
+++ kotlin/web-react/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+import kotlinx.browser.document
+
+fun main() {
+    document.bgColor = "blue"
+}
