874ce41438a5d490a20bae21397a9633fee3caa1
ilya-uzun <ilya.uzun@bk.ru>
2021-05-12 22:30:55 +0500

    update

--- kotlin/collection/coll.kt
+++
@@ -1,5 +0,0 @@
-
-class coll {
-    val list = listOf("111", "222", "3333")
-    val text = list.get(0)
-}
--- kotlin/collection/main.kt
+++ kotlin/collection/main.kt
@@ -1,10 +1,15 @@
 package com.kiparo
 
+// class coll {
+//     val list = listOf("111", "222", "3333")
+//     val text = list.get(0)
+// }
+
 fun main(args: Array<String>){
     println("ddd")
     // Колекции 
-     val list = listOf("111", "222", "3333")
-     val text = list.get(0)
-     println("Text = $text")
+     //val coll()
+   
+    // println("Text = $text")
 
 }
