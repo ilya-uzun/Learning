927c9ccced8021f55bf0d3f01d49b5302bbb0003
ilya-uzun <ilya.uzun@bk.ru>
2021-05-17 12:43:52 +0500

    Переменные и массивы

--- kotlin/Sudoku/src/main/kotlin/com/ilya/sudoku/main.kt
+++ kotlin/Sudoku/src/main/kotlin/com/ilya/sudoku/main.kt
@@ -2,5 +2,14 @@
 package com.ilya.sudoku
 
 fun main() {
-    println("test")
+    val firstNumber = 10.0
+    val secondNumber = 256
+    val therdNumder = firstNumber + secondNumber
+    println(therdNumder)
+    //вывод массива
+    val firstArray = arrayOf(1, 2, 34, 5)
+    println(firstArray.contentToString())
+
+    val modiArray = firstArray.map { number -> number * 10 }.toIntArray()
+    println(modiArray.contentToString())
 }
