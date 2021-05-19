667f7d4462e0d5a855d2db3291b270b65816c1bc
ilya-uzun <ilya.uzun@bk.ru>
2021-05-19 23:06:09 +0500

    Тест массивы

--- kotlin/Sudoku/src/main/kotlin/com/ilya/sudoku/main.kt
+++ kotlin/Sudoku/src/main/kotlin/com/ilya/sudoku/main.kt
@@ -6,14 +6,34 @@ import java.io.InputStream
 
 fun main() {
 
-    val input = readInput()
-//    val input: InputStream = File("input").inputStream()
-//    val inputString = input.bufferedReader().use { it.readText() }
-    println(input)
+    //val input = readInput()
+    //val input: InputStream = File("input").inputStream()
+    //val inputString = input.bufferedReader().use { it.readText() }
+    //println(input)
+
+    // Массивы тест
+    val x_1 = arrayOf(1,2)
+    val x_2 = arrayOf<Int>(1,2)
+    val x_3 = intArrayOf(1,2)
+    val x_4 = Array(6, {i -> i*1})
+    val x_5: Array<Int> = arrayOf(1,2)
+    val x_6: IntArray = intArrayOf(1,2)
+
+
+
+
+
+
+
+    println(x_1[1])
+    println(x_2[0])
+    println(x_3[0])
+    println(x_4[0])
+    println(x_5[0])
 }
 
 fun readInput():Map<Pair<Int, Int>, Int>{
-    
+
     val input = File("input")
         .readLines()
         .withIndex()
