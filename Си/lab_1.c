b66cedf16d80a4eae557b7e2b86d8955a5066b6b
ilya-uzun <ilya.uzun@bk.ru>
2021-06-18 17:21:36 +0500

    ФП лаб 1

---
+++ Си/lab_1.c
@@ -0,0 +1,13 @@
+main(){
+    int n =0, m=0;
+   int s = sum(n,m);
+ printf(s);
+}
+
+int sum (int n,int m){
+   int summa 0;
+   for (int i=0,n = i, i++)summa++;
+   if (n = summa){
+   for (int j=0,m = j, j++)summa++;
+   return summa;
+}
