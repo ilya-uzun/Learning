1af79e296597d20c0088dcdb039ba48813f3e981
ilya-uzun <ilya.uzun@bk.ru>
2021-06-28 20:11:24 +0500

    Задача матрица с случайными числами

--- Си/Matrix.c
+++ Си/Matrix.c
@@ -5,14 +5,23 @@
 int main()
 {
 	int i, j;
-	int myArray[2][3];
-	srand
+	int myArray[3][2];
+	srand(time(NULL)); // time(null)- выводит время в секундах о 1970г. srand() - при перезапускает rand() если изменён аргумент
 
-	for(i=0; i<2;i++){
-		for(j=0; j<3; j++)
+	for(i=0; i<3;i++){
+		for(j=0; j<2; j++)
 		{
 			myArray[i][j]= rand()%10;
 		}
 	}
-
+	// печать
+	for(i=0; i<3;i++){
+        printf("\n");
+		for(j=0; j<2; j++)
+		{
+			printf("%d ",myArray[i][j]);
+		}
+	}
+	printf("\n");
+		
 }
