84157b7bff41c16dd989fc679a0bcc86252de61e
ilya-uzun <ilya.uzun@bk.ru>
2021-09-19 13:15:07 +0500

    finish

--- C++/свой массив/main.cpp
+++ C++/свой массив/main.cpp
@@ -1,58 +1,49 @@
 #include <iostream>
 
-
-using namespace std;
-
 struct Array {
     int val;
     Array* previous;
 
-    Array(int val, Array* previous) : val(val), previous(previous){}
+    Array(int val, Array* previous) : val(val), previous(previous) {}
 
-    int& operator [](size_t index) 
-    {
-        if (index == 0){
-return val;
-        } 
-    
+    int& operator[](size_t index) {
+        if (index == 0) {
+            return val;
+        }
         return (*previous)[index - 1];
     }
-
 };
 
-void generator(size_t n, void(*previous)(Array), Array* previous = 0)
-{
+void generator(size_t n, void(*payload)(Array), Array* previous = 0) {
     Array element(0, previous);
-    if (n == 1)
-    {
+    if (n == 1) {
         payload(element);
-    } else 
-    {
+    } else {
         generator(n - 1, payload, &element);
     }
 }
- 
 
- size_t N;
+size_t N;
 
- void test(Array array)
- {
-     for (size_t i=0; i<N; ++i) cin >> array[i];
+void test(Array array) {
+    for (size_t i = 0; i < N; ++i) {
+        std::cin >> array[i];
+    }
+    for(size_t i = 0 ; i < N - 1; ++i) { 
+        for(size_t j = 0 ; j < N - i - 1 ; ++j) {  
+            if(array[j] > array[j+1]) {           
+                int tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp; 
+            }
+        }
+    }
+    for (size_t i = 0; i < N; ++i) {
+        std::cout << array[i] << ' ';
+    }
+}
 
-     for(size_t i = 0; i < N -1; ++i){
-         for(size_t j = 0; j < n - i - 1; ++i){
-             int(array[j]>array[j+1]){
-                 int tmp = array[j];
-                 array[j] = array[j+1];
-                 array[j+1] tmp;
-             }
-         }
-     }
-     for(size_t i=0; i < N; ++i) cout << array[i]<< ' ';
- }
-
-int main(){
-    cin>> N;
-    
-  return 0;
+int main() {
+    std::cin >> N;
+    generator(N, test);
 }
