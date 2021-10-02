74b7942101771efbf98d1822cdc574ae13c6c273
ilya-uzun <ilya.uzun@bk.ru>
2021-10-02 21:27:10 +0500

    Создал учебный проект

---
+++ Java/sarafan/src/test/java/com/uzun/sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzun.sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
