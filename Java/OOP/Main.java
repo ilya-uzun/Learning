dcd82bfc4fe80f0cabc3065901585229465db396
ilya-uzun <ilya.uzun@bk.ru>
2021-10-05 20:24:00 +0500

    Update

--- Java/OOP/Main.java
+++ Java/OOP/Main.java
@@ -1,31 +1,38 @@
-package ru.uzun;
 
 
-class  abstract Person {
+public abstract class Person {
     private String name = "Петя";
     private int age = 30;
     
-     public void ageM() {
+     public void ageM(){
        System.out.println(age);
    }
+   
+   public abstract void walk();
 }
 
 //класс-наследник Men переопределяет поведение метода walk() класса Person.
-class Men extends Person {
+public class Men extends Person {
+    @Override
     void walk(){
         System.out.println("Сообщение класса Men..");
     }
+}
 
-class Women extends Person {
-    void walk(){
-        System.out.println("Сообщение класса Women..");
-    }
-    
+ public class Women extends Person {
+     @Override
+     void walk(){
+         System.out.println("Сообщение класса Women..");
+     }
+ }
+
+public class Main {   
     public static void main(String[] arg){
-        Person p = new Men();
-        Person p2 = new Women();
+        Men p = new Men();
+        Women p2 = new Women();
         p.walk();
         p2.walk();
     }
+
 }
 //https://javarush.ru/groups/posts/1973-abstraktnihe-klassih-v-java-na-konkretnihkh-primerakh
